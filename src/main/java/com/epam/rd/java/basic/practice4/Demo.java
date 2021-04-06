03c87b53e16460886cbe91763ebd80bb0e67313d
Andrii Serha <sergaav@gmail.com>
2021-04-06 12:24:48 +0300

    Commit for Task1 and 2

--- src/main/java/com/epam/rd/java/basic/practice4/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Demo.java
@@ -3,7 +3,11 @@ package com.epam.rd.java.basic.practice4;
 public class Demo {
 
     public static void main(String[] args) {
+        System.out.println("=========================== PART1");
         Part1.main(args);
+
+        System.out.println("=========================== PART2");
+        Part2.main(args);
     }
 
 }
--- src/main/java/com/epam/rd/java/basic/practice4/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice4/Part2.java
@@ -2,18 +2,31 @@ package com.epam.rd.java.basic.practice4;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.Random;
 
 public class Part2 {
 
     public static void main(String[] args) {
-
-
+        String input;
+        StringBuilder stringBuilder = new StringBuilder();
+        createAndFillFile();
+        input = readFile(createAndFillFile()).trim();
+        String[] temp = input.split(" ");
+        int[] masInt = new int[temp.length];
+        for (int i = 0; i < masInt.length; i++) {
+            masInt[i] = Integer.parseInt(temp[i]);
+        }
+        sort(masInt);
+        for (int j : masInt) {
+            stringBuilder.append(j).append(" ");
+        }
+        writeFile(stringBuilder.substring(0,stringBuilder.lastIndexOf(" ")), "part2_sorted.txt");
+        System.out.print("input ==> "+ readFile("part2.txt"));
+        System.out.print("output ==> "+ readFile("part2_sorted.txt"));
     }
 
 
-    private static int[] sort(int[] arr) {
+    private static void sort(int[] arr) {
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -23,16 +36,52 @@ public class Part2 {
                 }
             }
         }
-        return arr;
     }
 
-    private static File createAndFillFile() {
-        File fileName = new File("part2.txt");
-        Charset charset = Charset.forName("cp1251");
-        if (!fileName.exists()){
-            fileName.
+    private static void writeFile(String s, String fileName) {
+        File file = new File(fileName);
+        boolean isExist = file.exists();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, Charset.forName("cp1251")))) {
+            if (!isExist) {
+                isExist = file.createNewFile();
+            }
+            if (isExist) {
+                bufferedWriter.write(s);
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    private static String readFile(String fileName) {
+        File file = new File(fileName);
+        StringBuilder stringBuffer = new StringBuilder();
+        boolean isExist = file.exists();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file, Charset.forName("cp1251")))) {
+            if (!isExist) {
+                isExist = file.createNewFile();
+            }
+            if (isExist) {
+                while (bufferedReader.ready()) {
+                    stringBuffer.append(bufferedReader.readLine()).append(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+
+    private static String createAndFillFile() {
+        StringBuilder stringBuffer = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            stringBuffer.append(new Random().nextInt(50)).append(" ");
+        }
+        String fileName = "part2.txt";
+        writeFile(stringBuffer.substring(0, stringBuffer.lastIndexOf(" ")), fileName);
+
+        return fileName;
     }
 
 }
